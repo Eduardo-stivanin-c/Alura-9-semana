ae0437ef63c0d8d8ca171d9205b151a427f081fe
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-05 18:16:18 -0300

    correção de erro

--- src/cli.js
+++ src/cli.js
@@ -6,13 +6,25 @@ import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-function imprimeLista(resultado) {
-    console.log(chalk.yellow('lista de links'), resultado)
+function imprimeLista(resultado,identificador='') {
+  console.log(chalk.yellow('lista de links'), resultado)
+ console.log(chalk.black.bgGreen(resultado,identificador))
+//}
+//texto preto grifado de verde
+
 }
 
 async function processaTexto(argumentos) {
     const caminho = argumentos[2];
-
+//tratanto erro
+    try {
+    fs.lstatSync(caminho);
+} catch (erro) {
+    if (erro.code ==='ENOENT') {
+        console.log('Arquivo ou diretorio inexitente');
+        return
+    } 
+}
     if (fs.lstatSync(caminho).isFile()) {
         const resultado = await pegaArquivo(argumentos[2]);
         imprimeLista(resultado);
@@ -20,14 +32,14 @@ async function processaTexto(argumentos) {
         const arquivos = await fs.promises.readdir(caminho)
             arquivos.forEach(async (nomeDeArquivo) => {
                 const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
-                imprimeLista(lista);
+                imprimeLista(lista,nomeDeArquivo);
             })
     }
 }
- //o primiero camnihoa apontoa prra 1 comanenste do Nojes Js e o segubdo aponta pra este aquivo
+ //o primeiro camihoa apontoa pra 1 comanenste do Noje Js e o segudo aponta pra este aquivo
 
  // o terceitor pé um texto passado
  
- //exigbe texto inserido comoa caminho nocomando 
+ //exige texto inserido comoa caminho no comando 
 
 await processaTexto(caminho);

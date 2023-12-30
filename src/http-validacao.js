8e7bdcf2f110cf66b325e4748d0900531e324596
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-12-30 11:21:19 -0300

    http via fetcjh usando sistemas de terceiros

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -3,12 +3,26 @@ function extraiLink(arryLinks) {
     //loop 
 return arryLinks.map((objetoLink) => Object.values(objetoLink).join());
     //loop pra pega somente os links numa unica matroiz o join juntas avariveas matrizes
+
     
 }
 
+//checa status se foir iencontrad ,ou naçao ,etc
+function checaStatus(listaURLs) {
+    
+}
+
+
+
+
 //valida ção comandoa cli  caminho --valida
-
-
 export function listaValidada(listaDeLinks) {
     return extraiLink(listaDeLinks);
 }
+
+//chamdno recuros de rerceiros do site do Node JS
+/*const res =await  fetch('https://nodejs.org/api/documentation.json');
+if (res.ok) {
+    const data=await res.json();
+    console.log(data);
+}*/
